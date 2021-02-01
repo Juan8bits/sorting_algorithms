d60a6b5af974237261957aed68165bf3262de794
EstephaniaCalvoC <2177@holbertonschool.com>
2021-02-01 09:24:25 -0500

    fix pointer

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,19 +6,19 @@
  * @n1: Pointer to first node.
  * @n2: Pointer to second node.
  */
-void swap_nds(listint_t **list, listint_t **n1, listint_t **n2)
+void swap_nds(listint_t **list, listint_t **n1, listint_t *n2)
 {
-	(*n1)->next = (*n2)->next;
-	if ((*n2)->next)
-		(*n2)->next->prev = *n1;
-	(*n2)->prev = (*n1)->prev;
-	(*n2)->next = *n1;
-	if ((*n1)->prev)
-		(*n1)->prev->next = *n2;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
 	else
-		*list = *n2;
-	(*n1)->prev = *n2;
-	*n1 = (*n2)->prev;
+		*list = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
@@ -40,7 +40,7 @@ void insertion_sort_list(listint_t **list)
 		prev = node->prev;
 		while (prev && node->n < prev->n)
 		{
-			swap_nds(list, &prev, &node);
+			swap_nds(list, &prev, node);
 			print_list((const listint_t *)*list);
 		}
 	}

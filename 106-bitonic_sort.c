46c0b00013e974aece1f53fbbe112bbac706e67a
Juan8bits <juan160820@gmail.com>
2021-02-03 23:49:18 +0000

    Bitonic sort algorithm unfinished

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+
+/**
+ * swap - Function that swap to integers.
+ * @n1: Integer number 1.
+ * @n2: Integer number 2.
+ */
+void swap(char mode, int *arr, size_t size)
+{
+	size_t act, jump = size / 2;
+
+	for (act = 0; act <= size; act += jump)
+	{	if (mode == 1 && arr[act] > arr[act + jump])
+		{
+			arr[act] = arr[act] ^ arr[act + jump];
+			arr[act + jump] = arr[act] ^ arr[act + jump];
+			arr[act] = arr[act] ^ arr[act + jump];
+		}
+	}
+}
+
+
+/**
+ * sort -
+ *
+ *
+ */
+void sort(char mode, int *arr, size_t size)
+{
+	if (size < 2)
+		return;
+	swap(mode, arr, size);
+	sort(mode, arr, size / 2);
+	sort(mode, arr + (size / 2), size / 2);
+}
+/**
+ * section_arr - Recursive function that sort array.
+ * @mode: UP(1)/DOWN(0) bitonic mode.
+ * @arr: Array to sort.
+ * @size: actual steps
+ * @S: Size of Original array.
+ */
+void section_arr(char mode, int *arr, size_t size, size_t S)
+{
+	if (size < 2)
+		return;
+	if (mode == 1)
+		printf("Merging [%lu/%lu] (UP):\n", size, S);
+	else
+		printf("Merging [%lu/%lu] (DOWN):\n", size, S);
+	print_array(arr, size);
+	section_arr(1, arr, size / 2, S);
+	section_arr(0, arr + (size / 2), size / 2, S);
+	sort(mode, arr, size);
+}
+
+/**
+ * bitonic_sort - Function that sorts an array of integers in ascending
+ * order using the Bitonic sort algorithm.
+ * @arr:
+ * @size:
+ */
+void bitonic_sort(int *arr, size_t size)
+{
+	if (!arr || size < 2)
+		return;
+	section_arr(1, arr, size, size);
+}
